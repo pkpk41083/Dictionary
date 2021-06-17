e9c89394e04c8843140335327840228932fbed24
Andy <pkpk41083@gmail.com>
2021-06-17 11:48:45 +0800

    Initial Commit

---
+++ SimpleDic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleDic
+//
+//  Created by Yukai Chang on 2021/6/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
